b9e02356e03c1b735c9b18e0a9a298fe16c51ff8
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-02-08 18:23:45 -0500

    commit tree 786adfb79503a9661d143a854047336308542ea5

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,8 @@
 # Create method `parrot` that outputs a given phrase and returns the phrase
-
-def parrot(sound="Squawk!")
-    puts sound
-    return sound
+def parrot(argueItem = "Squawk!")
+   puts argueItem
+   argueItem
 end
-parrot("Squawk")
 
-# def parrot(name="Pretty bird!")
-#     puts "#{name}"
-#     return "Pretty bird!"
-# end
 
+ 
